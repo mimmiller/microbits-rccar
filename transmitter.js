bb6bb067a65f4eabc10b068083834c8095646457
Mim Miller <millermiriamg@gmail.com>
2018-02-25 19:52:04 +1100

    less acceleration

--- transmitter.js
+++ transmitter.js
@@ -1,6 +1,14 @@
+let bb = ""
+let aa = ""
+let b = 0
+let speedB = 0
+let a = 0
+let speed = 0
+let speedA = 0
 let pressed = false
-let speedA = 1
-let speedB = 1
+pressed = false
+speedA = 1
+speedB = 1
 speedA = 1
 speedB = 1
 radio.setGroup(1)
@@ -8,36 +16,35 @@ radio.setTransmitPower(7)
 basic.forever(() => {
     if (input.buttonIsPressed(Button.AB)) {
         pressed = true
-        speedA = speedA * 2
-        if (speedA > 254) {
-            speedA = 254
+        speed = Math.max(speedA, speedB)
+        speed = speed * 2 - speed / 2
+        if (speed > 254) {
+            speed = 254
         }
-        speedB = speedB * 2
-        if (speedB > 254) {
-            speedB = 254
-        }
+        speedA = speed
+        speedB = speed
     } else if (input.buttonIsPressed(Button.A)) {
         pressed = true
-        speedA = speedA * 2
+        speedA = speedA * 2 - speedA / 2
         if (speedA > 254) {
             speedA = 254
         }
-        speedB = speedB / 2
+        speedB = speedA / 60
         if (speedB < 1) {
             speedB = 1
         }
     } else if (input.buttonIsPressed(Button.B)) {
         pressed = true
-        speedA = speedA / 2
+        speedA = speedB / 60
         if (speedA < 1) {
             speedA = 1
         }
-        speedB = speedB * 2
+        speedB = speedB * 2 - speedB / 2
         if (speedB > 254) {
             speedB = 254
         }
     } else if (pressed) {
-        radio.sendString("0:0")
+        radio.sendString("000:000")
         pressed = false
         speedA = 1
         speedB = 1
@@ -45,10 +52,8 @@ basic.forever(() => {
         led.unplot(4, 0)
     }
     if (pressed) {
-        let a = speedA - 1
-        let b = speedB - 1
-        let aa = ""
-        let bb = ""
+        a = speedA - 1
+        b = speedB - 1
         if (a < 10) {
             aa = "00" + a
         } else if (a < 100) {
@@ -63,8 +68,7 @@ basic.forever(() => {
         } else {
             bb = "" + b
         }
-        console.log(aa + ":" + bb)
-        radio.sendString(aa + ":" + bb)
+        radio.sendString("" + aa + ":" + bb)
         led.plotBrightness(0, 0, a)
         led.plotBrightness(4, 0, b)
     }
