0e0d0c115e17ed40ec4db09d7d08f65bfa19b16a
Mim Miller <millermiriamg@gmail.com>
2018-02-25 19:48:21 +1100

    simplied receiver - moved stuff to transmitter

--- receiver.js
+++ receiver.js
@@ -1,69 +1,21 @@
-let speedB = 0
-let pressedB = false
-let pressedA = false
-let speedA = 0
+let b = 0
+let a = 0
 radio.onDataPacketReceived(({ receivedString }) => {
     led.toggle(2, 2)
-    if (receivedString == "AB") {
-        pins.digitalWritePin(DigitalPin.P5, 0)
-        pins.digitalWritePin(DigitalPin.P11, 0)
-        led.unplot(1, 0)
-        led.plot(2, 0)
-        led.unplot(3, 0)
-    } else if (receivedString == "A") {
-        pins.digitalWritePin(DigitalPin.P5, 0)
-        pins.digitalWritePin(DigitalPin.P11, 1)
-        led.plot(1, 0)
-        led.unplot(2, 0)
-        led.unplot(3, 0)
-    } else if (receivedString == "B") {
-        led.unplot(1, 0)
-        led.unplot(2, 0)
-        led.plot(3, 0)
-        pins.digitalWritePin(DigitalPin.P5, 1)
-        pins.digitalWritePin(DigitalPin.P11, 0)
-    } else {
+    let aa = receivedString.substr(0, 3)
+    let bb = receivedString.substr(4, 7)
+    console.log("aa'" + aa + "' bb '" + bb + "'")
+    a = parseInt(aa)
+    b = parseInt(bb)
+    pins.servoSetPulse(AnalogPin.P0, a * 3000)
+    pins.servoSetPulse(AnalogPin.P1, b * 3000)
+    led.plotBrightness(0, 0, a)
+    led.plotBrightness(4, 0, b)
+    if (a == 0 && b == 0) {
         led.unplot(2, 2)
-        led.unplot(1, 0)
-        led.unplot(2, 0)
-        led.unplot(3, 0)
-        pins.digitalWritePin(DigitalPin.P5, 1)
-        pins.digitalWritePin(DigitalPin.P11, 1)
     }
 })
-speedA = 0
-speedB = 0
 pins.servoSetPulse(AnalogPin.P0, 0)
 pins.servoSetPulse(AnalogPin.P1, 0)
 pins.digitalWritePin(DigitalPin.P2, 1)
 radio.setGroup(1)
-basic.forever(() => {
-    if (input.buttonIsPressed(Button.A)) {
-        pressedA = true
-        speedA += 2000
-        if (speedA > 36000) {
-            speedA = 36000
-        }
-    } else if (pressedA) {
-        pressedA = false
-        speedA = 0
-    }
-    pins.servoSetPulse(AnalogPin.P0, speedA)
-    led.plotBrightness(0, 0, speedA / 100)
-    basic.pause(100)
-})
-basic.forever(() => {
-    if (input.buttonIsPressed(Button.B)) {
-        pressedB = true
-        speedB += 2000
-        if (speedB > 36000) {
-            speedB = 36000
-        }
-    } else if (pressedB) {
-        pressedB = false
-        speedB = 0
-    }
-    pins.servoSetPulse(AnalogPin.P1, speedB)
-    led.plotBrightness(4, 0, speedB / 100)
-    basic.pause(100)
-})
--- transmitter.js
+++ transmitter.js
@@ -1,31 +1,53 @@
 let pressed = false
+let speedA = 1
+let speedB = 1
+speedA = 1
+speedB = 1
 radio.setGroup(1)
 radio.setTransmitPower(7)
 basic.forever(() => {
     if (input.buttonIsPressed(Button.AB)) {
         pressed = true
-        radio.sendString("AB")
-        led.unplot(1, 0)
-        led.plot(2, 0)
-        led.unplot(3, 0)
+        speedA = speedA * 2
+        if (speedA > 254) {
+            speedA = 254
+        }
+        speedB = speedB * 2
+        if (speedB > 254) {
+            speedB = 254
+        }
     } else if (input.buttonIsPressed(Button.A)) {
         pressed = true
-        radio.sendString("A")
-        led.plot(1, 0)
-        led.unplot(2, 0)
-        led.unplot(3, 0)
+        speedA = speedA * 2
+        if (speedA > 254) {
+            speedA = 254
+        }
+        speedB = speedB / 2
+        if (speedB < 1) {
+            speedB = 1
+        }
     } else if (input.buttonIsPressed(Button.B)) {
         pressed = true
-        radio.sendString("B")
-        led.unplot(1, 0)
-        led.unplot(2, 0)
-        led.plot(3, 0)
+        speedA = speedA / 2
+        if (speedA < 1) {
+            speedA = 1
+        }
+        speedB = speedB * 2
+        if (speedB > 254) {
+            speedB = 254
+        }
     } else if (pressed) {
+        radio.sendString("0:0")
         pressed = false
-        radio.sendString("--")
-        led.unplot(1, 0)
-        led.unplot(2, 0)
-        led.unplot(3, 0)
+        speedA = 1
+        speedB = 1
+        led.unplot(0, 0)
+        led.unplot(4, 0)
+    }
+    if (pressed) {
+        radio.sendString((speedA - 1) + ":" + (speedB - 1))
+        led.plotBrightness(0, 0, speedA - 1)
+        led.plotBrightness(4, 0, speedB - 1)
     }
     basic.pause(100)
 })
